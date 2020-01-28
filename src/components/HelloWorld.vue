7ec6379a6476785f8e5a84e6c8e2a3051b402db2
yicj <626659321@qq.com>
2020-01-28 17:54:02 +0800

    保存

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <el-radio-group v-model="direction">
+      <el-radio label="ltr">从左往右开</el-radio>
+      <el-radio label="rtl">从右往左开</el-radio>
+      <el-radio label="ttb">从上往下开</el-radio>
+      <el-radio label="btt">从下往上开</el-radio>
+    </el-radio-group>
+
+    <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
+      点我打开
+    </el-button>
+
+    <el-drawer
+            title="我是标题"
+            :visible.sync="drawer"
+            :direction="direction"
+            :before-close="handleClose">
+      <span>我来啦!</span>
+    </el-drawer>
+
   </div>
 </template>
 
@@ -35,6 +29,17 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      drawer: false,
+      direction: 'rtl',
+    };
+  },
+  methods: {
+    handleClose(done) {
+        done() ;
+    }
   }
 }
 </script>
@@ -43,14 +48,6 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,44 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-//
-// Vue.config.productionTip = false
-//
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Button, Select,Option,
+  Dialog,DatePicker,Checkbox ,
+  Upload,Alert, Loading,Row,
+  Col, Icon,Input,MessageBox,
+  Notification,Message,Table, TableColumn,
+  Avatar,Drawer,Radio,RadioGroup
+} from 'element-ui';
 import App from './App.vue';
 
-Vue.use(ElementUI);
+Vue.config.productionTip = true ;
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Option) ;
+Vue.use(Dialog) ;
+Vue.use(DatePicker) ;
+Vue.use(Checkbox) ;
+Vue.use(Upload) ;
+Vue.use(Loading) ;
+Vue.use(Alert) ;
+Vue.use(Row) ;
+Vue.use(Col)
+Vue.use(Icon) ;
+Vue.use(Input) ;
+Vue.use(Table) ;
+Vue.use(TableColumn) ;
+Vue.use(Loading.directive);
+Vue.use(Avatar) ;
+Vue.use(Drawer) ;
+Vue.use(Radio) ;
+Vue.use(RadioGroup) ;
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
+
 
 new Vue({
   el: '#app',
